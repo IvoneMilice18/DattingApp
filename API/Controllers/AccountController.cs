ab5812cb3fb5f72579a1ed7b03df7a9d021e7c41
IvoneMilice18 <ivone@empirecybersecurity.co.mz>
2024-09-16 10:27:23 +0200

    end of section 6

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,15 +4,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController (DataContext context): BaseApiController 
+public class AccountController (DataContext context, ITokenService tokenService): BaseApiController 
 {
-[HttpPost("redister")]// account/register
-public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+[HttpPost("register")]// account/register
+public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
 {
     if (await UserExists(registerDto.Username)) return BadRequest("That User name is taken");
 
@@ -27,8 +28,38 @@ public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     context.Users.Add(user); 
     await context.SaveChangesAsync();
 
-    return user;
+    return new UserDto
+    {
+        Username = user.UserName,
+        Token = tokenService.CreateToken(user)
+    };
 }
+
+[HttpPost("login")]
+public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+{
+    var user = await context.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
+
+    if  (user == null) return Unauthorized("Invalid Username");
+
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+
+    var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+    for (int i = 0; i < computeHash.Length; i++)
+    {
+        if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+    }
+
+    return new UserDto
+    {
+        Username = user.UserName,
+        Token = tokenService.CreateToken(user)
+    };
+    
+}
+
+
 private async Task<bool> UserExists(string username)
 {
     return await context.Users.AnyAsync(x => x.UserName.ToLower()== username.ToLower());
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_service/account.service';
+import { NgIf, TitleCasePipe } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive,TitleCasePipe],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  accountSevice = inject(AccountService);
+  private router = inject(Router)
+  private toastr = inject(ToastrService);
+  model: any = {};
+
+  login() {
+    this.accountSevice.login(this.model).subscribe({
+      next: _ => {
+        this.router.navigateByUrl('/members')
+      },
+      error: error => this.toastr.error(error.error)
+    })
+  }
+
+  logout() {
+    this.accountSevice.logout();
+    this.router.navigateByUrl('/');
+  }
+}
